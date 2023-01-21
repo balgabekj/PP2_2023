9a34ad1bc5ca6ace4f0dc4d2a62e8cf62444ee78
balgabekj <123066464+balgabekj@users.noreply.github.com>
2023-01-21 14:50:14 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,36 @@
+# Python Syntax
+#Ex1
+print("Hello World")
+#Ex2
+if 5 > 2:
+    print("Five is greater than two!")
+# Python Comments
+#Ex1
+    #This is a comment
+#Ex2
+
+"""
+This is a comment
+written in 
+more that just one line
+
+"""
+# Python Variables
+#Ex1
+carname= "Volvo"
+#Ex2
+x = 50
+#Ex3
+
+x = 5
+y = 10
+print(x+y)
+
+#Ex4
+x = 5
+y = 10
+z = x + y
+print(z)
+#Ex5
+ethdfgh6
+
