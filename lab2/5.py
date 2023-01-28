ef23b24db7c76378f116f674f3e341c10c08b6ec
balgabekj <123066464+balgabekj@users.noreply.github.com>
2023-01-28 14:31:25 +0300

    Add files via upload

---
+++ lab2/5.py
@@ -0,0 +1,52 @@
+# Python if and else
+
+#Ex1
+
+a = 50
+b = 10
+if a > b:
+    print("Hello World")
+
+#Ex2
+a = 50
+b = 10
+if a != b:
+    print("Hello World")
+
+#Ex3
+a = 50
+b = 10
+if a ==b:
+    print("Yes")
+else:
+    print("No")
+
+#Ex4
+a = 50
+b = 10
+if a == b:
+  print("1")
+elif a > b:
+  print("2")
+else:
+  print("3")
+
+#Ex5
+
+if a == b and c == d:
+  print("Hello")
+
+#Ex6
+
+if a == b or c == d:
+    print("Hello")
+
+#Ex7
+if 5 > 2:
+  print("Five is greater than two!")
+
+#Ex8
+if 5 > 2:print("Five is greater than two!")
+
+#Ex9
+print("Yes") if 5 > 2 else print("No")
